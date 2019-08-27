5e8e3d4ca0477fa22d39df86f6ca612db61035ca
yusi64659779 <54586907+yusi64659779@users.noreply.github.com>
2019-08-27 13:39:00 -0400

    version 2 of Hello World

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display Hello World
+            Console.WriteLine("Press a key to continue..");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
